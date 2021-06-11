02580aa1afd2a5fa5f2e46b9942d8ef26ec310dd
Tamir <1tamir198@gmail.com>
2021-06-11 22:21:29 +0300

    Update code from laptop

---
+++ calculator=methods.js
@@ -0,0 +1,6 @@
+function numberButtonPressed(){
+        // console.log(document.getElementById("number1").textContent);
+        // document.getElementById("output").innerHTML = "button pressed - change output";
+        console.log("testing");
+}
+
