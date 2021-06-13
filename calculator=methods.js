74aadaa505743dda7f17054251a158dee88c26f5
Tamir <1tamir198@gmail.com>
2021-06-13 17:06:30 +0300

    Adding event listeners to all buttons

--- calculator=methods.js
+++ calculator=methods.js
@@ -1,6 +1,91 @@
-function numberButtonPressed(){
-        // console.log(document.getElementById("number1").textContent);
-        // document.getElementById("output").innerHTML = "button pressed - change output";
-        console.log("testing");
+window.onclick = e => {
+    let idOfPressedBtn = e.target.id;
+    let totalSum = 0;
+
+
+    switch (idOfPressedBtn) {
+        case "button1":
+            console.log(idOfPressedBtn);
+            break;
+        case "button2":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "button3":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "button4":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "button5":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "button6":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "button7":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "button8":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "button9":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "button0":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "deleteButton":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "plusButton":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "substructBUtton":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "decimalDot":
+            console.log('');
+            console.log(idOfPressedBtn);
+
+            break;
+        case "divideButton":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "multipleBUtton":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "resetButton":
+            console.log(idOfPressedBtn);
+
+            break;
+        case "equalButton":
+            console.log(idOfPressedBtn);
+
+            break;
+        default:
+            console.log('Button not found ' + idOfPressedBtn);
+    }
 }
+
+
+function numberButtonPressed() {
+    // console.log(document.getElementById("number1").textContent);
+    // document.getElementById("output").innerHTML = "button pressed - change output";
+    console.log("testing");
+}
+
 
